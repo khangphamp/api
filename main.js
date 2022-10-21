70bcb0903690e9311e4cfc076ae8c783b9bb498b
khangphamp <84113971+khangphamp@users.noreply.github.com>
2022-10-21 16:26:46 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const c =10000000;
+let array = Array(c)
+console.time("for")
+for(let i = 0; i <= c; i++){
+  // 
+}
+console.timeEnd("for")
+console.time("forEach")
+
+array.forEach(element => {
+  // 
+});
+console.timeEnd("forEach");
